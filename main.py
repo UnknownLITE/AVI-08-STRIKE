be59384bdbbdbc73eac8f8130cc3a28383e2b763
UnknownLITE <91004913+UnknownLITE@users.noreply.github.com>
2021-12-22 21:01:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 from datetime import datetime, time
 
 import nextcord
@@ -7,7 +8,7 @@ from bs4 import BeautifulSoup
 from nextcord.ext import commands, tasks
 from nextcord.utils import utcnow
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(">"), case_insensitive=True)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("~"), case_insensitive=True)
 
 
 @bot.event
@@ -50,16 +51,33 @@ async def Stats_Update():
     channel = bot.get_channel(916717503982493816)
     async with channel.typing():
         embed = nextcord.Embed(title=f"AOS Stats",
-                              description=create_embed("AOS"),
-                              color=0x00ff00)
+                               description=create_embed("AOS"),
+                               color=0x00ff00)
     await channel.send(embed=embed)
 
     channel = bot.get_channel(916717604159238214)
     async with channel.typing():
         embed = nextcord.Embed(title=f"STRIKE Stats",
-                              description=create_embed("STRIKE"),
-                              color=0x00ff00)
+                               description=create_embed("STRIKE"),
+                               color=0x00ff00)
     await channel.send(embed=embed)
     print(f"Stats Update Finished... for {datetime.now()}")
+
+
+@bot.command(aliases=["m", "member"])
+async def members(ctx, *, squad):
+    url = f"https://stats.warbrokers.io/squads/{squad}"
+    async with ctx.channel.typing():
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html.parser")
+        raw = soup.find_all("div", class_="squad-player-header")
+        e = nextcord.Embed(title=f"*`Members`*", colour=0x00ff00)
+        for i in range(len(raw)):
+            q = raw[i].text.replace("\n", "")
+            q = q.replace("\n", "")
+            e.set_author(name=f"[{squad}]", url=url, icon_url="https://warbrokers.io/img/ui_logo_200.png")
+            e.add_field(name="\u200B", value=f"{i + 1}. __[`{q}`](https://stats.warbrokers.io{raw[i].a['href']})__")
+            e.set_image(url=f"https://stats.warbrokers.io/images/backgrounds/banner-{random.randint(1, 20)}.jpg")
+    await ctx.send(embed=e)
 
 bot.run(os.environ["TOKEN"])
