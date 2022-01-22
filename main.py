3c92a6fe3f47c26d06deaed1740779881ed02105
UnknownLITE <unknownlite20@gmail.com>
2022-01-22 10:01:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,125 +1,170 @@
+"""
+Code a discord bot to keep servers clean.
+
+A bot to help keep Discord servers clean from profanity. Bot will search messages sent to channels for profanity, deleting profane text and timeout user for 5 min , logs banned users to a special channel.
+
+Bot will also automatically give users muted role (id: 933699268257144872) on their 5th offense.
+
+Add a slashcommand that would add or remove profanity from the filter, you may edit the en.txt file. To remove a word or phrase, delete it from the file; to add, add the new word or phrase on a new line.
+"""
+import json
+import re
+from datetime import datetime, timedelta, timezone
+from email.policy import default
+
+import hikari
+import lightbulb
 import os
-import random
-from datetime import datetime, time
 
-import nextcord
-import requests
-from bs4 import BeautifulSoup
-from nextcord.ext import commands, tasks
-from nextcord.utils import utcnow
-
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("~"), case_insensitive=True)
+bot = lightbulb.BotApp(os.environ['TOKEN'],
+                       default_enabled_guilds=834693207186341889)  # OTMzMjUzNDgwNDEwMzkwNTk4.Yee2CA.2p8QfaQAy24DRqF6IyRxWd5FRoM
 
 
-def get_stats(squad):
-    url = f"https://stats.warbrokers.io/squads/{squad}"
-
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    return soup
+@bot.listen(hikari.StartedEvent)
+async def on_started(event):
+    print(f'Logged in !!')
 
 
-@bot.event
-async def on_ready():
-    print(f"Logged on as {bot.user}|{bot.user.id}")
-    Stats_Update.start()
-
-
-def create_embed(squad):
-    soup = get_stats(squad)
-    raw = (soup.find("div", class_="squad-top-ten-weapons-grid")).text.replace("\n\n", "")
-    w = raw.split("\n")
-    q = {w[0]: w[1], w[2]: w[3], w[4]: w[5], w[6]: w[7], w[8]: w[9], w[10]: w[11], w[12]: w[13]}
-    tktd = (soup.find_all("div", class_="squad-player-stats"))
-    t, d, k = 0, 0, 0
-
-    for i in range(len(tktd)):
-        t += int(
-            tktd[i].find_all("div", class_="squad-player-number-box-value")[1].text.replace("\n", "").replace(",", ""))
-        d += int(
-            tktd[i].find_all("div", class_="squad-player-number-box-value")[2].text.replace("\n", "").replace(",", ""))
-    data = f"""```yml
-Total Kills:            {t}
-Total Deaths:           {d}
-Overall K/D:            {round(t / d, 1)}```
-```yml
-Deathmatch wins:        {q["Death Match"]}
-Missile Launch:         {q["Missile Launch"]}
-Battle Royale wins:     {q["Battle Royale"]}
-Package Drop:           {q["Package Drop"]}
-Vehicle escort:         {q["Vehicle Escort"]}
-Capture point:          {q["Capture Point"]}
-Zombie BR wins:         {q["Zombie BR"]}```"""
-    return data
-
-
-@tasks.loop(time=[time(hour=00, minute=00)])
-async def Stats_Update():
-    if utcnow().strftime("%A") != "Monday":
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def on_guild_message_create(event):
+    print(event.author, event.author.is_bot)
+    if event.author.is_bot and not (
+            hikari.Permissions.ADMINISTRATOR in lightbulb.utils.permissions.permissions_for(event.member)):
         return
-    print(f"Stats Update Started... for {datetime.now()}")
-    channel = bot.get_channel(898198669348712548)
-    async with channel.typing():
-        embed = nextcord.Embed(title=f"AOS Stats",
-                               description=create_embed("AOS"),
-                               color=0x00ff00,
-                               timestamp=utcnow())
-        embed.set_thumbnail(
-            url="attachment://logo.png"
-        )
-        image = nextcord.File("logo.png")
-        embed.set_author(name=f"[AOS]'s Stats", url=f"https://stats.warbrokers.io/squads/AOS")
-    await channel.send(
-        embed=embed,
-        file=image)
+    with open('en.txt', 'r', encoding='utf-8') as f:
+        profanity = f.read().strip('\ufeff').splitlines()
+    for word in profanity:
+        word = r'\b' + word + r'\b'
+        if re.search(word, event.message.content.lower()):
+            channel = await event.message.fetch_channel()
+            await event.message.delete()
+            embed = hikari.Embed(title='Profanity detected',
+                                 description=f'{event.author.mention}, you have been muted for 5 minutes.',
+                                 color=0xFF0000)
+            embed.timestamp = event.message.created_at
+            await channel.send(embed=embed)
 
-    channel = bot.get_channel(898198490759450654)
-    async with channel.typing():
-        embed = nextcord.Embed(description=create_embed("STRIKE"),
-                               color=0x00ff00,
-                               timestamp=utcnow())
-        embed.set_thumbnail(
-            url="attachment://logo.png"
-        )
-        image = nextcord.File("logo.png")
-        embed.set_author(name=f"[STRIKE]'s Stats", url=f"https://stats.warbrokers.io/squads/STRIKE")
-    await channel.send(
-        embed=embed,
-        file=image)
-    print(f"Stats Update Finished... for {datetime.now()}")
+            await event.member.edit(communication_disabled_until=datetime.now(timezone.utc) + timedelta(minutes=5))
 
+            # Logging
+            embed = hikari.Embed(title=f'Mute',
+                                 description=f'{event.author.mention} received a timeout of 5 minutes. \nReason: '
+                                             f'Profanity detected.',
+                                 color=0xFF0000)
+            embed.timestamp = event.message.created_at
+            embed.add_field(name='Message--', value=f"**in <#{event.channel_id}>**\n> {event.message.content}")
+            await bot.cache.get_guild_channel(
+                json.load(open('config.json', 'r'))['logging_channel'][str(event.guild_id)]).send(embed=embed)
+            log_file = json.load(open('warn_count.json', 'r'))
+            log_file[str(event.author.id)] = log_file.get(str(event.author.id), 0) + 1
+            json.dump(log_file, open('warn_count.json', 'w'), indent=4)
+            if log_file[str(event.author.id)] >= 5:
+                config = json.load(open('config.json', 'r'))
+                await event.member.edit(roles=[config["muted_role"][str(event.guild_id)]])
+                embed = hikari.Embed(title='Profanity Offense',
+                                     description=f'{event.author.mention} has been given <@&{config["muted_role"][str(event.guild_id)]}> for '
+                                                 f'their 6th profanity '
+                                                 f'offense.',
+                                     color=0xFF0000)
+                embed.set_author(name=f"{event.message.author.username}#{event.message.author.discriminator}")
+                embed.timestamp = event.message.created_at
 
-@bot.command()
-async def stats(ctx, *, squad):
-    async with ctx.channel.typing():
-        embed = nextcord.Embed(description=create_embed(f"{squad}"),
-                               color=0x00ff00,
-                               timestamp=utcnow())
-        embed.set_thumbnail(
-            url="attachment://logo.png"
-        )
-        image = nextcord.File("logo.png")
-        embed.set_author(name=f"[{squad}]'s Stats", url=f"https://stats.warbrokers.io/squads/{squad}")
-    await ctx.send(
-        embed=embed,
-        file=image)
+                await bot.cache.get_guild_channel(
+                    json.load(open('config.json', 'r'))['logging_channel'][str(event.guild_id)]).send(embed=embed)
+                log_file[str(event.author.id)] = 0
+                json.dump(log_file, open('warn_count.json', 'w'), indent=4)
+            return
 
 
-@bot.command(aliases=["m", "member"])
-async def members(ctx, *, squad):
-    url = f"https://stats.warbrokers.io/squads/{squad}"
-    async with ctx.channel.typing():
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, "html.parser")
-        raw = soup.find_all("div", class_="squad-player-header")
-        e = nextcord.Embed(title=f"*`Members`*", colour=0x00ff00, timestamp=utcnow())
-        for i in range(len(raw)):
-            q = raw[i].text.replace("\n", "")
-            q = q.replace("\n", "")
-            e.add_field(name="\u200B", value=f"{i + 1}. __[`{q}`](https://stats.warbrokers.io{raw[i].a['href']})__")
-        e.set_author(name=f"[{squad}]", url=url, icon_url="https://warbrokers.io/img/ui_logo_200.png")
-        e.set_image(url=f"https://stats.warbrokers.io/images/backgrounds/banner-{random.randint(1, 20)}.jpg")
-    await ctx.send(embed=e)
+@bot.listen(hikari.GuildMessageUpdateEvent)
+async def on_guild_message_update(event):
+    if event.author.is_bot and not (
+            hikari.Permissions.ADMINISTRATOR in lightbulb.utils.permissions.permissions_for(event.member)):
+        return
+    with open('en.txt', 'r') as f:
+        profanity = f.read().splitlines()
+    for word in profanity:
+        word = r'\b' + word + r'\b'
+        if re.search(word, event.message.content.lower()):
+            channel = await event.message.fetch_channel()
+            await event.message.delete()
+            embed = hikari.Embed(title='Profanity detected',
+                                 description=f'{event.author.mention}, you have been muted for 5 minutes.',
+                                 color=0xFF0000)
+            embed.timestamp = event.message.created_at
+            await channel.send(embed=embed)
+
+            await event.member.edit(communication_disabled_until=datetime.now(timezone.utc) + timedelta(minutes=5))
+
+            # Logging
+            embed = hikari.Embed(title=f'Mute',
+                                 description=f'{event.author.mention} received a timeout of 5 minutes. \nReason: '
+                                             f'Profanity detected.',
+                                 color=0xFF0000)
+            embed.timestamp = event.message.created_at
+            embed.add_field(name='Message--', value=f"**in <#{event.channel_id}>**\n> {event.message.content}")
+            await bot.cache.get_guild_channel(
+                json.load(open('config.json', 'r'))['logging_channel'][str(event.guild_id)]).send(embed=embed)
+            log_file = json.load(open('warn_count.json', 'r'))
+            log_file[str(event.author.id)] = log_file.get(str(event.author.id), 0) + 1
+            json.dump(log_file, open('warn_count.json', 'w'), indent=4)
+            if log_file[str(event.author.id)] >= 5:
+                config = json.load(open('config.json', 'r'))
+                await event.member.edit(roles=[config["muted_role"][str(event.guild_id)]])
+                embed = hikari.Embed(title='Profanity Offense',
+                                     description=f'{event.author.mention} has been given <@&{config["muted_role"][str(event.guild_id)]}> for '
+                                                 f'their 6th profanity '
+                                                 f'offense.',
+                                     color=0xFF0000)
+                embed.set_author(name=f"{event.message.author.username}#{event.message.author.discriminator}")
+                embed.timestamp = event.message.created_at
+            return
 
 
-bot.run(os.environ["TOKEN"])
+@bot.command
+@lightbulb.command('filter', 'Add/remove a word from the filter list.')
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def msg_filter(ctx):
+    pass
+
+
+@msg_filter.child
+@lightbulb.option('words', 'Word(s) to add to the profanity list.', type=str, required=True)
+@lightbulb.command('add', 'Add a word to the filter list.')
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def add_filter(ctx):
+    with open('en.txt', 'r') as f:
+        profanity = f.read().splitlines()
+    with open('en.txt', 'a') as f:
+        try:
+            w = ctx.options.words.lower().split(' ')
+            for i in w:
+                if w[i] in profanity:
+                    await ctx.reply(f'||{w[i]}|| is already in the filter list.')
+                    return
+        except TypeError:
+            f.write(f'{i}\n')
+            await ctx.respond(f'||{w}|| has been added to the filter list.')
+
+
+@msg_filter.child
+@lightbulb.command('remove', 'Remove a word from the filter list.')
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def remove_filter(ctx):
+    with open('en.txt', 'r') as f:
+        profanity = f.read().splitlines()
+    with open('en.txt', 'w') as f:
+        try:
+            w = ctx.options.words.lower().split(' ')
+            for i in w:
+                if w[i] not in profanity:
+                    await ctx.reply(f'||{w[i]}|| is not in the filter list.')
+                    return
+        except TypeError:
+            for i in profanity:
+                if i not in w:
+                    f.write(f'{i}\n')
+            await ctx.respond(f'||{w}|| has been removed from the filter list.')
+
+
+bot.run()
