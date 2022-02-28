89b1eb187d1e619bab9cdcc4cc6c82e95abb6542
UnknownLITE <unknownlite20@gmail.com>
2022-02-28 22:52:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,279 +1,116 @@
-"""
-Code a discord bot to keep servers clean.
-
-A bot to help keep Discord servers clean from profanity. Bot will search messages sent to channels for profanity, deleting profane text and timeout user for 5 min , logs banned users to a special channel.
-
-Bot will also automatically give users muted role on their 5th offense.
-
-Add a slash-command that would add or remove profanity from the filter, you may edit the en.txt file. To remove a word or phrase, delete it from the file; to add, add the new word or phrase on a new line.
-"""
-import json
-import re
-import time
-from datetime import datetime, timedelta, timezone
-import hikari
-import lightbulb
+import asyncio
 import os
+# import random
+from datetime import time # datetime, 
 
-bot = lightbulb.BotApp(os.environ['TOKEN'], prefix='=')
+import aiomysql
+import nextcord
+import requests
+from bs4 import BeautifulSoup
+from nextcord.ext import commands, tasks
+# from nextcord.utils import utcnow
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("."), case_insensitive=True,
+                   intents=nextcord.Intents.all())
+
+bot.remove_command("help")
+
+def get_stats(squad):
+    url = f"https://stats.warbrokers.io/squads/{squad}"
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    return soup
 
 
-@bot.listen(hikari.StartedEvent)
-async def on_started(event):
-    print(f'=====\nLogged in as {bot.get_me().username} | {bot.get_me().id}\n=====')
-    global uptime
-    uptime = time.time()
-
-@bot.command
-@lightbulb.command(name="uptime", description="Get the bot's uptime.")
-@lightbulb.implements(lightbulb.PrefixCommand)
-async def uptime_cmd(ctx):
-    y = str(timedelta(seconds=int(round(time.time() - uptime))))
-    e = hikari.Embed(title='Uptime',
-                     description=f'{y}',
-                     color=0x00ff00)
-    e.timestamp = datetime.now(timezone.utc)
-    await ctx.respond(embed=e)
-
+@bot.event
+async def on_ready():
+    print(f"Logged on as {bot.user}|{bot.user.id}")
+    # Stats_Update.start()
+    update_nick.start()
     
-@bot.listen(hikari.GuildMessageCreateEvent)
-async def on_guild_message_create(event):
-    if event.author.is_bot or (
-            hikari.Permissions.ADMINISTRATOR in lightbulb.utils.permissions.permissions_for(event.member)
-    ) or event.author.is_system:
+def nick_scrap(link):
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text, "html.parser")
+    raw = soup.find("div", class_="page-header")
+    if len(raw.text) > 2:
+        nick = raw.text.replace("  ", "").split("\n")[1]
+        return nick
+    else:
+        return "Invalid"
+
+
+@bot.command(aliases=["nick"])
+async def link(ctx, stats_link, member: nextcord.Member = None):
+    if member is not None and not ctx.author.guild_permissions.administrator and not ctx.author.id == member.id or member.bot:
+        await ctx.message.add_reaction("🚫")
         return
-    with open('en.txt', 'r', encoding='utf-8') as f:
-        profanity = f.read().strip('\ufeff').splitlines()
-    pd = []
-    for word in profanity:
-        w = r'\b' + word + r'+\b'
-        if re.search(w, event.message.content.lower()):
-            pd.append(word)
-    if len(pd) == 0:
-        return
-    await event.message.delete()
-    channel = await event.message.fetch_channel()
-    embed = hikari.Embed(title='Profanity detected',
-                         description=f'{event.author.mention}, you have been muted for 5 minutes.',
-                         color=0xFF0000)
-    embed.timestamp = event.message.created_at
-    await channel.send(embed=embed)
+    if member is None or member.id == ctx.author.id:
+        member = ctx.author
+    if stats_link.startswith("https://stats.warbrokers.io/players/i/"):
+        url = stats_link
+    else:
+        url = f"https://stats.warbrokers.io/players/i/{stats_link}"
+    with ctx.channel.typing():
+        # r = requests.get(url)
+        if nick_scrap(url) == "Invalid":
+            msg = await ctx.send(content=f"Invalid :x:")
+            await ctx.message.add_reaction("❌")
+            await asyncio.sleep(1)
+            await msg.delete()
+            return
+        else:
+            await member.edit(nick=nick_scrap(url))
+            await ctx.message.add_reaction("✅")
+        # msg = await ctx.send(f"Validating stats link...")
+        # match r.status_code:
+        #     case 200:
+        #         result = "Valid :white_check_mark:"
+        #     case _:
+        #         result =
 
-    await event.member.edit(communication_disabled_until=datetime.now(timezone.utc) + timedelta(minutes=5))
+        #         return
+        # await msg.edit(content=f"{result}")
 
-    # Logging
+        # t
 
-    embed = hikari.Embed(title=f'Mute',
-                         description=f'{event.author.mention} received a timeout of 5 minutes. \nReason: '
-                                     f'Profanity detected.',
-                         color=0xFF0000)
-    embed.timestamp = event.message.created_at
-    embed.add_field(name='Message:', value=f"**in <#{event.channel_id}>**\n> {event.message.content}")
-    embed.add_field(name='Word(s) detected:', value=f"||{pd}||")
-    await bot.cache.get_guild_channel(
-        json.load(open('config.json', 'r'))['logging_channel'][str(event.guild_id)]).send(embed=embed)
-    log_file = json.load(open('warn_count.json', 'r'))
-    log_file[str(event.author.id)] = log_file.get(str(event.author.id), 0) + 1
-    json.dump(log_file, open('warn_count.json', 'w'), indent=4)
-    if log_file[str(event.author.id)] >= 5:
-        config = json.load(open('config.json', 'r'))
-        msg_create.info(f'{event.author.username}#{event.author.discriminator} ({event.author.id}) received a '
-                        f'Muted role ({config["muted_role"][str(event.guild_id)]})')
-        await event.member.edit(roles=[config["muted_role"][str(event.guild_id)]])
-        embed = hikari.Embed(title='Profanity Offense',
-                             description=f'{event.author.mention} has been given <@&{config["muted_role"][str(event.guild_id)]}> for '
-                                         f'their 6th profanity '
-                                         f'offense.',
-                             color=0xFF0000)
-        embed.set_author(name=f"{event.message.author.username}#{event.message.author.discriminator}")
-        embed.timestamp = event.message.created_at
+        # await msg.delete()
 
-        await bot.cache.get_guild_channel(
-            json.load(open('config.json', 'r'))['logging_channel'][str(event.guild_id)]).send(embed=embed)
-        log_file[str(event.author.id)] = 0
-        json.dump(log_file, open('warn_count.json', 'w'), indent=4)
+    conn = await aiomysql.connect(host='remotemysql.com', port=3306,
+                                  user='PhVgPxDJdd', password='OWKglFnGUr', db='PhVgPxDJdd')
+
+    cur = await conn.cursor()
+    await cur.execute("SELECT * FROM WB_Nickname;")
+    for i in await cur.fetchall():
+        if i[0] == ctx.author.id:
+            await cur.execute(f"UPDATE WB_Nickname SET player_id = '{url[-24:]}' WHERE user_id = {member.id};")
+            await conn.commit()
+            await cur.close()
+            return
+    await cur.execute(
+        f"INSERT INTO `WB_Nickname` (`user_id`, `player_id`) VALUES ('{member.id}', '{url[-24:]}');")
+    await conn.commit()
+    await cur.close()
     return
 
 
-@bot.listen(hikari.GuildMessageUpdateEvent)
-async def on_guild_message_update(event):
-    if event.author.is_bot or (
-            hikari.Permissions.ADMINISTRATOR in lightbulb.utils.permissions.permissions_for(event.member)
-    ) or event.author.is_system:
-        return
-    with open('en.txt', 'r') as f:
-        profanity = f.read().splitlines()
-    pd = []
-    for word in profanity:
-        w = r'\b' + word + r'+\b'
-        if re.search(w, event.message.content.lower()):
-            pd.append(word)
-    if len(pd) == 0:
-        return
-    await event.message.delete()
-    embed = hikari.Embed(title='Profanity detected',
-                         description=f'{event.author.mention}, you have been muted for 5 minutes.',
-                         color=0xFF0000)
-    embed.timestamp = event.message.created_at
-    channel = await event.message.fetch_channel()
-    await channel.send(embed=embed)
-    await event.member.edit(communication_disabled_until=datetime.now(timezone.utc) + timedelta(minutes=5))
-
-    # Logging
-
-    embed = hikari.Embed(title=f'Mute',
-                         description=f'{event.author.mention} received a timeout of 5 minutes. \nReason: '
-                                     f'Profanity detected.',
-                         color=0xFF0000)
-    embed.timestamp = event.message.created_at
-    embed.add_field(name='Message:', value=f"**in <#{event.channel_id}>**\n> {event.message.content}")
-    embed.add_field(name='Word(s) detected:', value=f"||{pd}||")
-    await bot.cache.get_guild_channel(
-        json.load(open('config.json', 'r'))['logging_channel'][str(event.guild_id)]).send(embed=embed)
-    log_file = json.load(open('warn_count.json', 'r'))
-    log_file[str(event.author.id)] = log_file.get(str(event.author.id), 0) + 1
-    json.dump(log_file, open('warn_count.json', 'w'), indent=4)
-    if log_file[str(event.author.id)] >= 5:
-        config = json.load(open('config.json', 'r'))
-        await event.member.edit(roles=[config["muted_role"][str(event.guild_id)]])
-        embed = hikari.Embed(title='Profanity Offense',
-                             description=f'{event.author.mention} has been given <@&{config["muted_role"][str(event.guild_id)]}> for '
-                                         f'their 6th profanity '
-                                         f'offense.',
-                             color=0xFF0000)
-        embed.set_author(name=f"{event.message.author.username}#{event.message.author.discriminator}")
-        embed.timestamp = event.message.created_at
-
-        await bot.cache.get_guild_channel(
-            json.load(open('config.json', 'r'))['logging_channel'][str(event.guild_id)]).send(embed=embed)
-        log_file[str(event.author.id)] = 0
-        json.dump(log_file, open('warn_count.json', 'w'), indent=4)
-    return
-
-
-@bot.command
-@lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.ADMINISTRATOR))
-@lightbulb.command(name="filter", description="Filter a word from the server.")
-@lightbulb.implements(lightbulb.PrefixCommandGroup)
-async def msg_filter(ctx):
-    pass
-
-
-@msg_filter.child
-@lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.ADMINISTRATOR))
-@lightbulb.option("word", "Word to ban", str, required=True)
-@lightbulb.command("add", "Adds a word to the filter list.")
-@lightbulb.implements(lightbulb.PrefixSubCommand)
-async def add_filter(ctx: lightbulb.Context) -> None:
-    with open('en.txt', 'r') as f:
-        profanity = f.read().splitlines()
-    if ctx.options.word.lower() in profanity:
-        await ctx.respond(f'{ctx.author.mention}, that word is already in the filter list.')
-        return
-    with open('en.txt', 'a') as f:
-        f.write(f'{ctx.options.word.lower()}\n')
-        e = hikari.Embed(title='Word added to filter list',
-                         description=f'{ctx.author.mention}, ||{ctx.options.word.lower()}|| has been added to the '
-                                     f'filter list.',
-                         color=hikari.Color.from_hex_code('#57F287'))
-        e.timestamp = datetime.now(timezone.utc)
-        await ctx.respond(embed=e)
-
-
-@msg_filter.child
-@lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.ADMINISTRATOR))
-@lightbulb.option('word', 'string to ban', type=str, required=True)
-@lightbulb.command('remove', 'Remove a word from the filter list.')
-@lightbulb.implements(lightbulb.PrefixSubCommand)
-async def remove_filter(ctx):
-    with open('en.txt', 'r') as f:
-        profanity = f.read().splitlines()
-    if ctx.options.word.lower() not in profanity:
-        await ctx.respond(f'{ctx.author.mention}, that word is not in the filter list.')
-        return
-    with open('en.txt', 'r') as f:
-        profanity = f.read().splitlines()
-    with open('en.txt', 'w') as f:
-        for word in profanity:
-            if word != ctx.options.word.lower():
-                f.write(f'{word}\n')
-    e = hikari.Embed(title='Word removed from filter list',
-                     description=f'{ctx.author.mention}, ||{ctx.options.word.lower()}|| has been removed from the '
-                                 f'filter '
-                                 f'list.',
-                     color=hikari.Color.from_hex_code('#FEE75C'))
-    e.timestamp = datetime.now(timezone.utc)
-    await ctx.respond(embed=e)
-
-
-@bot.listen(lightbulb.CommandErrorEvent)
-async def on_error(event: lightbulb.CommandErrorEvent) -> None:
-    if isinstance(event.exception, lightbulb.CommandInvocationError):
-        await event.context.respond(
-            f"Something went wrong during invocation of command `{event.context.command.name}`.")
-        raise event.exception
-
-    # Unwrap the exception to get the original cause
-    exception = event.exception.__cause__ or event.exception
-
-    if isinstance(exception, lightbulb.NotOwner):
-        await event.context.respond("You are not the owner of this bot.")
-    elif isinstance(exception, lightbulb.MissingRequiredPermission):
-        pass
-    elif ...:
-        ...
+@link.error
+async def chnick_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f"Missing argument :warning:\nRun `{ctx.prefix}nick [link_to_stats/player_id]`")
     else:
-        raise exception
+        raise error
 
 
-@bot.command
-@lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.ADMINISTRATOR))
-@lightbulb.option('member', 'Of whom?', type=hikari.Member, required=True)
-@lightbulb.command('reset_warn_count', 'Resets warn counts for a user', aliases=["rwc"])
-@lightbulb.implements(lightbulb.PrefixCommand)
-async def reset_warn_count(ctx):
-    if ctx.options.member.id == ctx.author.id:
-        await ctx.respond(f'{ctx.author.mention}, you can\'t reset your own warn count.')
-        return
-    if ctx.options.member.is_bot or (
-            hikari.Permissions.ADMINISTRATOR in lightbulb.utils.permissions.permissions_for(ctx.options.member)):
-        await ctx.respond(f'{ctx.author.mention}, {ctx.options.member.mention} can\'t have warnings.')
-        return
-    warn_counts = json.load(open('warn_count.json', 'r'))
-    if str(ctx.options.member.id) in warn_counts:
-        del warn_counts[str(ctx.options.member.id)]
-        await ctx.respond(f'{ctx.author.mention}, the warn count for {ctx.options.member.mention} has been reset.')
-    else:
-        await ctx.respond(f'{ctx.author.mention}, {ctx.options.member.mention} has no warnings.')
-    json.dump(warn_counts, open('warn_count.json', 'w'), indent=4)
+@tasks.loop(time=[time(hour=00, minute=00)])
+async def update_nick():
+    conn = await aiomysql.connect(host='remotemysql.com', port=3306,
+                                  user='PhVgPxDJdd', password='OWKglFnGUr', db='PhVgPxDJdd')
+
+    cur = await conn.cursor()
+    await cur.execute("SELECT * FROM WB_Nickname;")
+    for i in await cur.fetchall():
+        await bot.get_guild(867986226458808360).get_member(int(i[0])).edit(nick=nick_scrap('https://stats.warbrokers.io/players/i/' + i[1]))
 
 
-@bot.command
-@lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.ADMINISTRATOR))
-@lightbulb.option('member', 'Of whom?', type=hikari.Member, required=True)
-@lightbulb.command('warn_count', 'Show warn counts for a user', aliases=["wc"])
-@lightbulb.implements(lightbulb.PrefixCommand)
-async def warn_count(ctx):
-    if ctx.options.member.id == ctx.author.id:
-        await ctx.respond(f'{ctx.author.mention}, you can\'t check your own warn count.')
-        return
-    if ctx.options.member.is_bot or (
-            hikari.Permissions.ADMINISTRATOR in lightbulb.utils.permissions.permissions_for(ctx.options.member)):
-        await ctx.respond(f'{ctx.author.mention}, {ctx.options.member.mention} can\'t have warnings.')
-        return
-    warn_counts = json.load(open('warn_count.json', 'r'))
-    if not str(ctx.options.member.id) in warn_counts:
-        color = hikari.Color.from_hex_code('#57F287')
-    elif 0 < warn_counts[str(ctx.options.member.id)] < 4:
-        color = hikari.Color.from_hex_code('#FEE75C')
-    else:
-        color = hikari.Color.from_hex_code('#ED4245')
-    embed = hikari.Embed(title=f'Warn count(s)',
-                         description=f'{ctx.options.member.mention} has {warn_counts.get(str(ctx.options.member.id), "no")}'
-                                     f' warning(s).',
-                         color=color)
-    embed.timestamp = datetime.now(timezone.utc)
-    await ctx.respond(embed=embed)
-
-
-bot.run()
+bot.run(os.environ['TOKEN'])
